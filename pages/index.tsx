22e5e774850b5e1caffa2e323838fee51e7667e2
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-10-03 15:17:30 +0100

    Add header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,43 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import {
+  FiBookOpen,
+  FiChevronDown,
+  FiChevronsRight,
+  FiUnlock,
+} from "react-icons/fi";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div className="bg-white">
+      <header className="flex items-center justify-between p-2 text-[#a9aeb8]">
+        <div className="flex items-center gap-4">
+          <FiChevronsRight className="text-[#6c727f]" />
+          <p className="flex items-center gap-2 text-sm">
+            <span className="flex items-center gap-2 font-medium underline transition-all cursor-pointer hover:underline">
+              <FiBookOpen /> Main
+            </span>
+            <span>/</span>
+            <span className="transition-all cursor-pointer hover:underline">
+              Getting Started
+            </span>
+            <span>/</span>
+            <span>Front-end developer test proje...</span>
+          </p>
         </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+        <div className="flex items-center gap-4 p-2">
+          <div className="flex items-center gap-1 text-sm font-normal">
+            <span>
+              <FiUnlock />
+            </span>
+            <span>Editing</span>
+          </div>
+          <button className="text-[#3665a9] font-bold text-sm tracking-wider flex items-center gap-2">
+            Publish Space <FiChevronDown />
+          </button>
+        </div>
+      </header>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
