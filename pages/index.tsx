06b2475dfd721e8d740689a8d148bd50bc8cda2c
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-10-06 00:12:43 +0100

    add a new way of input

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,81 +1,44 @@
 import type { NextPage } from "next";
 import Header from "../components/Header";
 import Statusbar from "../components/Statusbar";
-import { useEffect, useState } from "react";
-import Block from "../components/Block";
+import { useEffect, useRef, useState } from "react";
 import Overlay from "../components/Overlay";
 import uniqid from "uniqid";
 
 const Home: NextPage = () => {
-  const [blocks, setBlocks] = useState([] as any[]);
+  const [innerHTML, setInnerHTML] = useState(
+    `<h1>Front-end developer test project</h1>
+    <p class='m-0'>Your goal is to make a page that looks exactly like this one, and has the ability to create H1 text simply by typing / then 1, then typing text, and hitting enter.</p>`
+  );
   const [command, setCommand] = useState("");
-  const [inputValue, setInputValue] = useState("");
+
+  const Ref = useRef(null as any);
 
   useEffect(() => {
-    setBlocks([
-      {
-        id: uniqid(),
-        type: "p",
-        content:
-          "Your goal is to make a page that looks exactly like this one, and has the ability to create H1 text simply by typing / then 1, then typing text, and hitting enter.",
-      },
-    ]);
-  }, []);
+    if (Ref.current.innerHTML !== innerHTML) Ref.current.innerHTML = innerHTML;
+  }, [innerHTML]);
 
   const addBlock = (e: any, type: string, content = "") => {
     e.preventDefault();
-
-    setBlocks([...blocks, { id: uniqid(), type, content }]);
-    setInputValue("");
-    setCommand("");
-  };
-
-  const removeBlock = (e: any, isDivider?: boolean) => {
-    if (e.target.innerText === "" || isDivider) {
-      setBlocks(blocks.filter((b: any) => b.id !== e.target.id));
-    }
+    setInnerHTML(innerHTML + `<${type}>${content}</${type}>`);
   };
 
   return (
-    <div className="p-2 pb-32 bg-white noscroll">
+    <div className="p-2 pb-32 bg-white">
       <Header />
       <main className="mx-auto max-w-[700px] mt-4">
         <Statusbar />
+        <div
+          ref={Ref}
+          contentEditable="true"
+          className="mt-8 prose outline-none max-w-none min-w-screen"
+          onInput={(e: any) => {
+            setInnerHTML(e.target.innerHTML);
+          }}
+        ></div>
         <form className="flex flex-col w-full gap-4 mt-8">
-          <h1 className="text-4xl font-bold">
-            Front-end developer test project
-          </h1>
-          <hr />
           <div className="flex flex-col gap-6 text-[#4e5663] ">
-            {blocks.map(block => (
-              <Block
-                block={block}
-                id={block.id}
-                key={block.id}
-                removeBlock={removeBlock}
-                addBlock={addBlock}
-                setCommand={setCommand}
-              />
-            ))}
-            <input
-              placeholder="Type / for blocks, @ to link docs or people"
-              className="w-full outline-none text-[#5d6470] font-medium"
-              onKeyDown={(e: any) => {
-                if (e.key === "Enter") {
-                  addBlock(e, "p", e.target.value);
-                  e.target.value = "";
-                }
-              }}
-              onChange={(e: any) => {
-                setInputValue(e.target.value);
-
-                const regex = /\/\S{0,}/gm;
-                const command = e.target.value.match(regex);
-                command ? setCommand(command[0]) : setCommand("");
-              }}
-              value={inputValue}
-            />
-            {command && <Overlay addBlock={addBlock} command={command} />}
+            {<Overlay addBlock={addBlock} command={command} />}
           </div>
         </form>
       </main>
