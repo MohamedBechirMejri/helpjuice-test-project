3e0e2c63f0d90f73e73e83b717342ebe6cb21a3f
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-10-05 11:02:47 +0100

    style element based on type

--- components/Block.tsx
+++ components/Block.tsx
@@ -9,14 +9,32 @@ const Block = ({
   id: string;
   removeBlock: any;
 }) => {
+  const h1 = "text-4xl font-bold";
+  const h2 = "text-2xl font-bold";
+  const h3 = "text-xl font-semibold";
+
+  let type = "";
+
+  switch (block.type) {
+    case "h1":
+      type = h1;
+      break;
+    case "h2":
+      type = h2;
+      break;
+    case "h3":
+      type = h3;
+      break;
+  }
+
   return (
     <div
       contentEditable="true"
-      className="outline-none"
+      className={`outline-none ${type}`}
       onKeyDown={removeBlock}
       id={id}
     >
-      {block.content}
+      {block.content} tet
     </div>
   );
 };
