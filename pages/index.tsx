d81d77b5c54fa3c7ef1bbb9163203d283e4d23d7
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-10-06 11:47:58 +0100

    add new element below the caret

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,28 @@
 import type { NextPage } from "next";
 import Header from "../components/Header";
 import Statusbar from "../components/Statusbar";
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import Overlay from "../components/Overlay";
-import getCaretIndex from "../libs/getCaretIndex";
 import getCaretCoordinates from "../libs/getCaretCoordinates";
+import getCaretElement from "../libs/getCaretElement";
 
 const Home: NextPage = () => {
-  const [innerHTML, setInnerHTML] = useState(
-    `<h1>Front-end developer test project</h1>
-    <p class='m-0'>Your goal is to make a page that looks exactly like this one, and has the ability to create H1 text simply by typing / then 1, then typing text, and hitting enter.</p>`
-  );
   const [command, setCommand] = useState("");
   const [caretCoordinates, setCaretCoordinates] = useState({
     x: 0,
     y: 0,
   });
-
-  const Ref = useRef(null as any);
-
-  useEffect(() => {
-    if (Ref.current.innerHTML !== innerHTML) Ref.current.innerHTML = innerHTML;
-  }, [innerHTML]);
+  const [caret, setCaret] = useState(null as any);
 
   const addElement = (e: any, type: string, content = "") => {
     e.preventDefault();
-    setInnerHTML(innerHTML + `<${type}>${content}</${type}>`);
+    if (caret) {
+      // @ts-ignore
+      caret.focusNode.parentElement.insertAdjacentHTML(
+        "afterend",
+        `<${type}>${content}</${type}>`
+      );
+    }
     setCommand("");
   };
 
@@ -35,16 +32,15 @@ const Home: NextPage = () => {
       <main className="mx-auto max-w-[700px] mt-4">
         <Statusbar />
         <div
-          ref={Ref}
           contentEditable="true"
           className="mt-8 prose outline-none max-w-none min-w-screen"
-          onInput={(e: any) => {
-            setInnerHTML(e.target.innerHTML);
+          onBlur={e => {
+            // prevent focus loss when clicking overlay
+            e.target.focus();
           }}
           onKeyDown={(e: any) => {
-            // getCaretIndex(e.target);
+            setCaret(getCaretElement());
             if (command) {
-              e.preventDefault();
               // only add letters or numbers
               if (e.key.length === 1) setCommand(command + e.key);
               if (e.key === "Backspace")
@@ -52,12 +48,19 @@ const Home: NextPage = () => {
               if (e.key === "Escape") setCommand("");
             }
             if (e.key === "/") {
-              e.preventDefault();
               setCommand("/");
             }
-            setCaretCoordinates(getCaretCoordinates());
+            const coords = getCaretCoordinates();
+            coords.x && coords.y && setCaretCoordinates(coords);
           }}
-        ></div>
+        >
+          <h1>Front-end developer test project</h1>
+          <p className="m-0">
+            Your goal is to make a page that looks exactly like this one, and
+            has the ability to create H1 text simply by typing / then 1, then
+            typing text, and hitting enter.
+          </p>
+        </div>
         {command && (
           <Overlay
             addElement={addElement}
