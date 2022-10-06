bf624080a03f2b0e526a79d7cade19e2c18a9090
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-10-06 15:47:18 +0100

    remove comments

--- components/Overlay.tsx
+++ components/Overlay.tsx
@@ -35,16 +35,6 @@ const Overlay = ({
       title: "Heading 3",
       hint: "Small section heading",
     },
-    // {
-    //   type: "checkbox",
-    //   title: "Checkbox",
-    //   hint: "Todo list item",
-    // },
-    // {
-    //   type: "hr",
-    //   title: "Divider",
-    //   hint: "Visually divide blocks",
-    // },
   ];
   return (
     <div
